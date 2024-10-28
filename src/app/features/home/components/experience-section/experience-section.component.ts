3ef59f58deaa7fe7d1f9a9755e587c2bddf0ee1b
felipe.dev <083felype@gmail.com>
2024-10-28 18:42:23 -0300

    feat: add experience-section component

---
+++ src/app/features/home/components/experience-section/experience-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-experience-section',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './experience-section.component.html',
+  styleUrl: './experience-section.component.scss'
+})
+export class ExperienceSectionComponent {
+
+}
